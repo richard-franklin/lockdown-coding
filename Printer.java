13ca1095c6a0687c92afaa982c670f35795bae11
Richard <53473010+richard3658@users.noreply.github.com>
2020-05-18 20:40:50 +0530

    Add files via upload
    Write down a java program to print even and odd numbers series respectively
    from two threads: t1 and t2 synchronizing on a shared object
    Take as command line arguments, the following inputs to the program:
    Sleep Interval for thread t1
    Sleep Interval for thread t2
    Message per cycle
    No of cycles

---
+++ Printer.java
@@ -0,0 +1,82 @@
+class PrintEvenTask implements Runnable{
+    Printer printer;
+    int max;
+    PrintEvenTask(Printer printer, int max){
+        this.printer = printer;
+        this.max = max;
+    }
+    @Override
+    public void run() {
+        for(int i = 2; i <= max; i+=2){ 
+		       
+            printer.printEven(i);
+        }   
+    }
+}
+ 
+class PrintOddTask implements Runnable{
+    Printer printer;
+    int max;
+    PrintOddTask(Printer printer, int max){
+        this.printer = printer;
+        this.max = max;
+    }
+    @Override
+    public void run() {
+        for(int i = 1; i <= max; i+=2){
+            printer.printOdd(i);
+        }   
+    }
+}
+ 
+public class Printer {
+    boolean evenFlag = false;
+	public int sleep1,sleep2;
+    public void printEven(int num){
+        synchronized (this) {
+            while(!evenFlag){
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Thread Interrupted" + e.getMessage());
+                }
+            }
+            System.out.println(Thread.currentThread().getName()+"-->"+ num);
+            evenFlag = false;
+		try{
+		Thread.sleep(sleep2);
+		}
+		catch(Exception e){}
+		notify();
+        }
+    }
+    public void printOdd(int num){
+        synchronized (this) {
+            while(evenFlag){
+                try {
+			wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Thread Interrupted" + e.getMessage());
+                }
+            }
+            System.out.println(num+"--"+Thread.currentThread().getName());
+            evenFlag = true;
+		try{
+		Thread.sleep(sleep1);
+		}
+		catch(Exception e){}
+		notify();
+        }
+    }
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+	PrintOddTask po=new PrintOddTask(printer,Integer.parseInt(args[0]));
+	PrintEvenTask pe=new PrintEvenTask(printer,Integer.parseInt(args[0]));
+	printer.sleep1=Integer.parseInt(args[1]);
+	printer.sleep2=Integer.parseInt(args[2]);
+        Thread t1 = new Thread(po, args[3]);
+        Thread t2 = new Thread(pe, args[4]);
+        t1.start();
+        t2.start();
+    }
+}
