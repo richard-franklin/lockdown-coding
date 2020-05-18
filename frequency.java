6c2ef654467fc99704591b539ee026c4e20fa814
Richard <53473010+richard3658@users.noreply.github.com>
2020-05-18 18:58:22 +0530

    Create frequency.java
    Program 1:
    
    Using methods charAt() & length() of String class, write a program to print thefrequency of each character in a string.
    example:
    “Hello friend”
    
    Output should be
    
    ' ': 1
    
    'd': 1
    
    'e': 2
    
    'f': 1
    
    (continued for all character in the string)

---
+++ frequency.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Online{
+     public static void main(String[] args) {
+        String str;
+        Scanner s=new Scanner(System.in);
+        str=s.next();
+        char ele;
+        int i=0,j=0;
+        for(i= 0 ;i<str.length();i++)
+        {
+            int c = 0;
+            ele = str.charAt(i);
+            if (ele != '-')
+            {
+                for(j= i ;j<str.length();j++)
+                {
+                    if (ele == str.charAt(j))
+                        c++;
+                    
+                }
+                str = str.replace(ele, '-');
+                System.out.println(ele+" "+c);
+            }  
+        }
+    }
+}
